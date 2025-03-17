a39ffb6168dc0fc53975b1b9adf6b17e340656ea
Goddaybc <goddayoghenerobiz@gmail.com>
2025-03-17 21:43:40 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
